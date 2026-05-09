0086155a4ecdd8ee1e3057ce5a18d71d1d74c1f1
abhinavusar <abhinavusar2029@gmail.com>
2026-05-10 02:56:29 +0530

    reverse star triangle pattern program using twin loops nesting
    Implement a program to print a reverse star triangle pattern based on user input for the number of rows.

---
+++ star_triangle_reverse.cpp
@@ -0,0 +1,16 @@
+//star cross
+#include<iostream>
+using namespace std;
+int main(){
+    int row;
+    cout<<"Number of rows:";cin>>row;
+    for(int i=0;i<row;i++){
+        for(int j=1;j<(row-i);j++){
+            cout<<"  ";
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
